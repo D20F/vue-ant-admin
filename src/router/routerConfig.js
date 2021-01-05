307ce5c2dba2bdef3229adbfe586e09f19444464
fb <2442563148@qq.com>
2021-01-05 16:46:38 +0800

    :poor:

--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -100,6 +100,35 @@ export const example = [
         ]
     }
 ]
+export const component = [
+    {
+        path: '/component',
+        component: Layout,
+        redirect: '/component/table',
+        name: 'component',
+        meta: { title: '组件', icon: 'el-icon-s-help' },
+        children: [
+            {
+                path: 'login',
+                name: 'login',
+                component: () => import('@/views/component/login/index'),
+                meta: { title: '登录', icon: 'table' }
+            },
+            {
+                path: 'card',
+                name: 'card',
+                component: () => import('@/views/component/card/index'),
+                meta: { title: '卡片', icon: 'table' }
+            },
+            {
+                path: 'success',
+                name: 'success',
+                component: () => import('@/views/component/success/index'),
+                meta: { title: '成功页面', icon: 'table' }
+            },
+        ]
+    }
+]
 
 // 表单
 export const form = [
@@ -107,14 +136,14 @@ export const form = [
         path: '/form',
         component: Layout,
         children: [
-          {
-            path: 'index',
-            name: 'Form',
-            component: () => import('@/views/form/index'),
-            meta: { title: 'Form', icon: 'form' }
-          }
+            {
+                path: 'index',
+                name: 'Form',
+                component: () => import('@/views/form/index'),
+                meta: { title: 'Form', icon: 'form' }
+            }
         ]
-      },
+    },
 ]
 
 // 树
@@ -125,89 +154,90 @@ export const nested = [
         redirect: '/nested/menu1',
         name: 'Nested',
         meta: {
-          title: 'Nested',
-          icon: 'nested'
+            title: 'Nested',
+            icon: 'nested'
         },
         children: [
-          {
-            path: 'menu1',
-            component: () => import('@/views/nested/menu1/index'), // Parent router-view
-            name: 'Menu1',
-            meta: { title: 'Menu1' },
-            children: [
-              {
-                path: 'menu1-1',
-                component: () => import('@/views/nested/menu1/menu1-1'),
-                name: 'Menu1-1',
-                meta: { title: 'Menu1-1' }
-              },
-              {
-                path: 'menu1-2',
-                component: () => import('@/views/nested/menu1/menu1-2'),
-                name: 'Menu1-2',
-                meta: { title: 'Menu1-2' },
+            {
+                path: 'menu1',
+                component: () => import('@/views/nested/menu1/index'), // Parent router-view
+                name: 'Menu1',
+                meta: { title: 'Menu1' },
                 children: [
-                  {
-                    path: 'menu1-2-1',
-                    component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                    name: 'Menu1-2-1',
-                    meta: { title: 'Menu1-2-1' }
-                  },
-                  {
-                    path: 'menu1-2-2',
-                    component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                    name: 'Menu1-2-2',
-                    meta: { title: 'Menu1-2-2' }
-                  }
+                    {
+                        path: 'menu1-1',
+                        component: () => import('@/views/nested/menu1/menu1-1'),
+                        name: 'Menu1-1',
+                        meta: { title: 'Menu1-1' }
+                    },
+                    {
+                        path: 'menu1-2',
+                        component: () => import('@/views/nested/menu1/menu1-2'),
+                        name: 'Menu1-2',
+                        meta: { title: 'Menu1-2' },
+                        children: [
+                            {
+                                path: 'menu1-2-1',
+                                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                                name: 'Menu1-2-1',
+                                meta: { title: 'Menu1-2-1' }
+                            },
+                            {
+                                path: 'menu1-2-2',
+                                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                                name: 'Menu1-2-2',
+                                meta: { title: 'Menu1-2-2' }
+                            }
+                        ]
+                    },
+                    {
+                        path: 'menu1-3',
+                        component: () => import('@/views/nested/menu1/menu1-3'),
+                        name: 'Menu1-3',
+                        meta: { title: 'Menu1-3' }
+                    }
                 ]
-              },
-              {
-                path: 'menu1-3',
-                component: () => import('@/views/nested/menu1/menu1-3'),
-                name: 'Menu1-3',
-                meta: { title: 'Menu1-3' }
-              }
-            ]
-          },
-          {
-            path: 'menu2',
-            component: () => import('@/views/nested/menu2/index'),
-            name: 'Menu2',
-            meta: { title: 'menu2' }
-          }
+            },
+            {
+                path: 'menu2',
+                component: () => import('@/views/nested/menu2/index'),
+                name: 'Menu2',
+                meta: { title: 'menu2' }
+            }
         ]
-      },
+    },
 ]
- 
+
 // link 跳转
 export const link = [
     {
         path: 'external-link',
         component: Layout,
         children: [
-          {
-            path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-            meta: { title: 'External Link', icon: 'link' }
-          }
+            {
+                path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+                meta: { title: 'External Link', icon: 'link' }
+            }
         ]
-      },
+    },
 ]
 
 
 // root 账户
-export const root = [...err,...example,...form,...nested,...link]
+export const root = [...err, ...component, ...example, ...form, ...nested, ...link]
 
 // 运营商
-export const operator = [...err,...form,...nested]
+export const operator = [...err, ...form, ...nested]
 
 
 // 全部暴露
 export const ALL = {
-    basis:basis,
-    example:example,
-    form:form,
-    nested:nested,
-    link:link,
-    root:root,
-    operator:operator,
+    basis: basis,
+    component: component,
+    example: example,
+    form: form,
+    nested: nested,
+    link: link,
+    root: root,
+    operator: operator,
 }
---
+++ src/views/component/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dashboard">
+        
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+    name: "Dashboard",
+    computed: {
+        ...mapGetters(["name"]),
+    },
+    methods: {},
+    created() {},
+};
+</script>
+
+<style lang="scss" scoped>
+$subMenuHover: #1890ff;
+.dashboard {
+    width: 100%;
+
+}
+</style>
